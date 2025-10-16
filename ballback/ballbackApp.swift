62ae3dc558657f2620b9d09f2005b6857f87758a
Powloux <jamesperrow96@gmail.com>
2025-10-16 10:35:53 -0600

    Initial Commit

---
+++ ballback/ballbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ballbackApp.swift
+//  ballback
+//
+//  Created by James Perrow on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ballbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
